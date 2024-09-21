e73c0f80522efdafc793192f03f03972db23ff2c
sergey-frey <sergey.frey.work@gmail.com>
2024-09-21 21:45:29 +0300

    init: start astro project

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://sergey-frey.github.io",
+  base: "typing-flow-demo",
+});
